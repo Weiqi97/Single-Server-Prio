006a9539257ac9ccec13ff0c697fc4fcaba1de45
Weiqi <weltch1997@gmail.com>
2023-06-26 13:19:12 -0400

    Update

--- FLPCP/flpcp.go
+++ FLPCP/flpcp.go
@@ -1,11 +1,11 @@
 package FLPCP
 
 import (
-	"crypto/rand"
 	"github.com/Weiqi97/Single-Server-Prio/utilities"
-	"github.com/fentec-project/gofe/data"
 	"github.com/fentec-project/gofe/innerprod/simple"
+	"math"
 	"math/big"
+	"math/rand"
 )
 
 type FLPCP struct {
@@ -19,43 +19,61 @@ func InitFLPCP(vectorLength int, params *simple.DDHParams) (scheme FLPCP) {
 	return
 }
 
-func (scheme FLPCP) prover(x data.Vector) (pi data.Vector) {
+func (scheme FLPCP) prover(x []float64) (pi []float64) {
 	// The prover output pi = (w, f_1(0), ..., f_L(0), c_p), where L = 3.
-	var p = make([]*big.Int, scheme.N+1)
+	var p = make([]float64, scheme.N+1)
 
 	// W is the witness, which we randomly sample from Zp.
-	var w *big.Int
-	w, _ = rand.Int(rand.Reader, scheme.P)
+	w := float64(rand.Int())
 	pi = append(pi, w)
 
 	// f_i(j) is the i-th input wired to j-th G gate, we have n + 1 G gate, where |x| = n.
-	var f10, f20, f30 *big.Int
-	f10, _ = rand.Int(rand.Reader, scheme.P)
-	f20, _ = rand.Int(rand.Reader, scheme.P)
-	f30, _ = rand.Int(rand.Reader, scheme.P)
+	f10 := float64(rand.Int())
+	f20 := float64(rand.Int())
+	f30 := float64(rand.Int())
 	pi = append(pi, f10, f20, f30)
 
 	// Useful values for the circuit.
-	var one, r, temp, output *big.Int
-	one.SetInt64(1)
-	r, _ = rand.Int(rand.Reader, scheme.P)
+	r := float64(rand.Int())
 
 	// p(j) is the value on the output wire from the j-th G-gate.
 	for i := 0; i < scheme.N; i++ {
-		temp.Sub(x[i], one)
-		p[i].Mul(x[i], temp)
+		p[i] = (x[i] - 1) * x[i]
 	}
 
+	output := 0.0
+
 	// The final output G gate.
-	output = utilities.SumVector(scheme.N, p)
-	temp.Sub(w, r)
-	p[scheme.N+1].Mul(output, temp)
+	for i := 0; i < scheme.N; i++ {
+		output += p[i]
+	}
+	p[scheme.N+1] = (w - r) * output
 
 	// So the degree is N + 1.
+	coeffs := utilities.LinSolver(scheme.N+1, p)
+
+	pi = append(pi, coeffs...)
 
 	return
 }
 
-func (scheme FLPCP) verifier() {
+func (scheme FLPCP) verifier(pi []float64) (b float64) {
+	// Extract the variables.
+	w := pi[0]
+	f10 := pi[1]
+	f20 := pi[2]
+	f30 := pi[3]
+	coeffs := pi[4:]
 
+	// Execute the first check. There's j polynomials need to be created.
+
+	// Execute the second check.
+	b = 0.0
+	for i := 0; i < scheme.N; i++ {
+		b += math.Pow(float64(scheme.N), float64(i)) * coeffs[i]
+	}
+	// Add the constant term.
+	b += coeffs[len(coeffs)-1]
+
+	return
 }
--- utilities/helper.go
+++ utilities/helper.go
@@ -1,6 +1,11 @@
 package utilities
 
-import "math/big"
+import (
+	"fmt"
+	"gonum.org/v1/gonum/mat"
+	"math"
+	"math/big"
+)
 
 // GetStandardBasis returns a vector of desired length where it is zero everywhere and one at index.
 func GetStandardBasis(length int, index int) (e []*big.Int) {
@@ -18,10 +23,47 @@ func GetStandardBasis(length int, index int) (e []*big.Int) {
 	return
 }
 
+// SumVector sums all elements in a vector.
 func SumVector(length int, x []*big.Int) (sum *big.Int) {
 	sum = x[0]
 	for i := 1; i < length; i++ {
 		sum.Add(sum, x[i])
 	}
+	return
+}
+
+// LinSolver finds the solution of Ax = b to x.
+func LinSolver(numGate int, y []float64) (x []float64) {
+	matrixA := mat.NewDense(numGate, numGate+1, nil)
+
+	// Fill in the matrix.
+	for i := 0; i < numGate; i++ {
+		for j := 0; j < numGate; j++ {
+			matrixA.Set(i, j, math.Pow(float64(i), float64(j)))
+		}
+	}
+
+	// Add 1 for the constant.
+	for i := 0; i < numGate; i++ {
+		matrixA.Set(i, numGate, 1.0)
+	}
+
+	// Convert y values to vec.
+	yVec := mat.NewVecDense(numGate, y)
+
+	// Create the coefficients.
+	coeffs := mat.NewVecDense(numGate+1, nil)
+
+	// Solve the linear system.
+	err := coeffs.SolveVec(matrixA, yVec)
+
+	// Error handling.
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Assign values and return.
+	x = coeffs.RawVector().Data
 	return
 }
